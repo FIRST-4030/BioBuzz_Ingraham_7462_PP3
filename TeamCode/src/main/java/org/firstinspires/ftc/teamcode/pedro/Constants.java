3d460fde3d4de95550baf8f53e74773b6727a2a0
Laptop29 <jello.schaefer@gmail.com>
2026-09-24 17:00:46 -0700

    Finished PEdro Pathing 3 configuration for FTC-QQYR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
 import com.pedropathing.algorithm.Foresight;
+import com.pedropathing.algorithm.ForesightConfig;
+import com.pedropathing.controllers.Controller;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Matrix;
+import com.pedropathing.math.Vector2D;
 import com.pedropathing.revhub.drivetrains.Mecanum;
 import com.pedropathing.revhub.drivetrains.MecanumConfig;
 import com.pedropathing.revhub.localizers.PinpointConfig;
@@ -14,12 +18,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static Follower create(HardwareMap h) {
-//        return new Follower(
-//                new PinpointLocalizer(h, localizerConfig),
-//                new Mecanum(h, drivetrainConfig),
-//                new Foresight(foresightConfig)
-//        );
-        return null;
+        return new Follower(
+                new PinpointLocalizer(h, localizerConfig),
+                new Mecanum(h, drivetrainConfig),
+                new Foresight(foresightConfig)
+        );
     }
 
     public static MecanumConfig drivetrainConfig = new MecanumConfig(c -> {
@@ -38,9 +41,35 @@ public class Constants {
         c.podType.set(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
         c.xPodOffset.set(5.51542267085999);
         c.yPodOffset.set(7.078430295929196);
-        c.xPodDirection.set(GoBildaPinpointDriver.EncoderDirection.FORWARD);
-        c.yPodDirection.set(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        c.xPodDirection.set(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        c.yPodDirection.set(GoBildaPinpointDriver.EncoderDirection.REVERSED);
         c.globalDistanceUnit.set(DistanceUnit.INCH);
         c.offsetUnits.set(DistanceUnit.INCH);
     });
+
+    public static ForesightConfig foresightConfig = new ForesightConfig(
+            c -> {
+                Controller primaryTranslationalForward = Controller.proportional(0.18086664661892252);
+                Controller secondaryTranslationalForward = Controller.proportional(0.06682539307988643);
+                Controller primaryTranslationalLateral = Controller.proportional(0.2366904539691113);
+                Controller secondaryTranslationalLateral = Controller.proportional(0.08745079825617685);
+
+                c.forwardTranslational.set(Controller.piecewise(secondaryTranslationalForward).put(2.5, primaryTranslationalForward));
+                c.strafeTranslational.set(Controller.piecewise(secondaryTranslationalLateral).put(2.5, primaryTranslationalLateral));
+
+                c.coast.set(Controller.proportionalFeedforward(0.017695350265048596));
+                c.brake.set(Controller.proportionalFeedforward(0.015041047725291306));
+
+                c.headingFeedback.set(Controller.proportional(2.2074264436605504));
+                c.headingBrakeCoefficients.set(Vector2D.cartesian(0.05811908353651974, 0.006156975220540176));
+
+                c.linearBrakeCoefficients.set(Matrix.diag(0.08960296027681121, 0.0493170673853901));
+                c.quadraticBrakeCoefficients.set(Matrix.diag(0.0012808566672233232, 0.0022722629641428183));
+
+                c.maxAchievableForwardVelocity.set(58.688846163761845);
+                c.maxAchievableStrafeVelocity.set(49.85006241750271);
+                c.naturalForwardDeceleration.set(38.03189062532916);
+                c.naturalStrafeDeceleration.set(52.92498883862722);
+            }
+    );
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.algorithm.Foresight;
 import com.pedropathing.revhub.drivetrains.Mecanum;
 import com.pedropathing.revhub.localizers.PinpointLocalizer;
 import com.pedropathing.tuning.autotune.Procedure;
@@ -21,14 +22,15 @@ public class Tuning {
     public static Procedure pinpointTuner(){
         return new PinpointTuner();
     }
-//    @Tuner
-//    public static Procedure twoWheelTuner() {
-//        return new TwoWheelTuner();
-//    }
+
     @Tuner
     public static Procedure tests() {
-        return new Tests(hardwareMap -> new Mecanum(hardwareMap, Constants.drivetrainConfig), (hardwareMap -> new PinpointLocalizer(hardwareMap, Constants.localizerConfig)), null);
+        return new Tests(hardwareMap -> new Mecanum(hardwareMap,
+                Constants.drivetrainConfig),
+                (hardwareMap -> new PinpointLocalizer(hardwareMap, Constants.localizerConfig)),
+                () -> new Foresight(Constants.foresightConfig));
     }
+
     @Tuner
     public static Procedure foresightTuner() {
         return new ForesightTuner((hardwareMap) -> new PinpointLocalizer(hardwareMap, Constants.localizerConfig), (hardwareMap) -> new Mecanum(hardwareMap, Constants.drivetrainConfig));
